1d1a29c0b6e44dffab85e247ab03f5bb991c3f44
ndn1665 <ndn1665@daum.net>
2024-02-08 16:48:11 +0900

    room id 1 fix

--- catchme/mysite/room/urls.py
+++ catchme/mysite/room/urls.py
@@ -5,6 +5,6 @@ app_name = "room"
 urlpatterns = [
     path('api/room_info/',room_views.RoomListView.as_view(), name="roomListView"),
     path('',room_views.index,name ="index"),
-    path('<int:r_no>/',room_views.selectedRoom,name="selectedRoom"),
+    path('1/',room_views.selectedRoom,name="selectedRoom"),
     path('api/<int:r_no>/',room_views.SelectedRoomView.as_view(), name= "selectedRoomView"),
 ]
