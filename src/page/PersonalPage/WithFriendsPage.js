e82bb7919f49ea1cb0d82d534b71d46531dae4b1
YeonUkKim <kimyeonuk0120@gamil.com>
2024-02-08 18:24:26 +0900

    [연욱 간편친구추가]

--- src/page/PersonalPage/WithFriendsPage.js
+++ src/page/PersonalPage/WithFriendsPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Modal from "react-modal";
+import "./WithFriendsPage.css";
 
 const friends = [
   {
@@ -8,70 +9,28 @@ const friends = [
     nickname: "w98_hyun_",
     gender: "남",
     age: "26",
-    loacte: "강남구 전체",
+    locate: "강남구 ",
   },
   {
     id: 2,
     nickname: "JIPDANJISUNG",
     gender: "여",
     age: "24",
-    loacte: "강남구 전체",
+    locate: "강남구 ",
   },
   {
-    id: 1,
+    id: 3,
     nickname: "w99_hyun_",
     gender: "남",
     age: "26",
-    loacte: "강남구 전체",
+    locate: "강남구 ",
   },
   {
-    id: 2,
+    id: 4,
     nickname: "JIPDANJISUNG",
     gender: "여",
     age: "24",
-    loacte: "강남구 전체",
-  },
-  {
-    id: 1,
-    nickname: "w99_hyun_",
-    gender: "남",
-    age: "26",
-    loacte: "강남구 전체",
-  },
-  {
-    id: 2,
-    nickname: "JIPDANJISUNG",
-    gender: "여",
-    age: "24",
-    loacte: "강남구 전체",
-  },
-  {
-    id: 1,
-    nickname: "w99_hyun_",
-    gender: "남",
-    age: "26",
-    loacte: "강남구 전체",
-  },
-  {
-    id: 2,
-    nickname: "JIPDANJISUNG",
-    gender: "여",
-    age: "24",
-    loacte: "강남구 전체",
-  },
-  {
-    id: 1,
-    nickname: "w99_hyun_",
-    gender: "남",
-    age: "26",
-    loacte: "강남구 전체",
-  },
-  {
-    id: 2,
-    nickname: "JIPDANJISUNG",
-    gender: "여",
-    age: "24",
-    loacte: "강남구 전체",
+    locate: "강남구 ",
   },
 ];
 
@@ -168,20 +127,154 @@ const styles = {
     maxHeight: "75vh",
   },
 };
+const modalStyles = {
+  overlay: {
+    backgroundColor: "transparent", //주변 음영 지우기
+  },
+};
 
 function WithFriends() {
-  const [modalIsOpen, setModalIsOpen] = useState("false");
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [locate, setLocate] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [univ, setUniv] = useState("");
+  const [body, setBody] = useState("");
+
+  let index = friends.length;
+  const handleRegister = () => {
+    // 콘솔에 값들을 출력
+    const addFriend = {
+      id: ++index,
+      nickname: name,
+      gender: "남",
+      age: age,
+      locate: locate,
+    };
+    console.log("이름:", name);
+    console.log("나이:", age);
+    console.log("거주지:", locate);
+    console.log("닮은 동물 상:", animal);
+    console.log("대학/학과:", univ);
+    console.log("키/체형:", body);
+    setModalIsOpen(false);
+
+    friends.push(addFriend);
+    setName(null);
+    setAge(null);
+    setAnimal(null);
+    setLocate(null);
+    setUniv(null);
+    setBody(null);
+  };
+
   const navigate = useNavigate();
-  const friendClick = function () {};
+  const simpleFriendPlus = function () {
+    setModalIsOpen(true);
+  };
   return (
     <div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        contentLabel="Modal"
+        style={modalStyles}
+        className="sfp-detail"
+      >
+        <div className="sfp-container">
+          <div className="sfp-sfp">간편 친구 추가</div>
+          <div className="sfp-info-container">
+            <div className="sfp-info-holder">
+              <span>이름:</span>
+              <input
+                id="name"
+                type="text"
+                placeholder=" ex) 홍길동"
+                className="sfp-name-input"
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div className="sfp-info-age-locate-container">
+              <div className="sfp-info-holder">
+                <span>나이:</span>
+                <input
+                  id="age"
+                  type="number"
+                  placeholder=" ex) 21"
+                  className="sfp-age-input"
+                  onChange={(e) => setAge(e.target.value)}
+                />
+              </div>
+              <div className="sfp-info-holder">
+                <span>거주지:</span>
+                <input
+                  id="locate"
+                  type="text"
+                  placeholder=" ex) 마포구"
+                  className="sfp-locate-input"
+                  onChange={(e) => setLocate(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="sfp-info-holder">
+              <span>닮은 동물 상:</span>
+              <input
+                id="animal"
+                type="text"
+                placeholder=" ex) 펭귄상"
+                className="sfp-animal-input"
+                onChange={(e) => setAnimal(e.target.value)}
+              />
+            </div>
+            <div className="sfp-info-holder">
+              <span>대학/학과:</span>
+              <input
+                id="univ"
+                type="text"
+                placeholder=" ex) 홍익대 / 컴퓨터공학과"
+                className="sfp-univ-input"
+                onChange={(e) => setUniv(e.target.value)}
+              />
+            </div>
+            <div className="sfp-info-holder">
+              <span>키/체형:</span>
+              <input
+                id="body"
+                type="text"
+                placeholder=" ex) 150 / 통통"
+                className="sfp-body-input"
+                onChange={(e) => setBody(e.target.value)}
+              />
+            </div>
+          </div>
+          <div>
+            <span>
+              <button onClick={handleRegister} className="sfp-regist-button">
+                등록하기
+              </button>
+            </span>
+            <span>
+              <button
+                onClick={() => setModalIsOpen(false)}
+                className="sfp-cancel-button"
+              >
+                취소
+              </button>
+            </span>
+          </div>
+        </div>
+      </Modal>
       <div style={styles.withfriendsHeader}>
         <div>
           <span style={styles.withfriendsText}>친구목록</span>
         </div>
         <div style={{ flexGrow: 1 }}></div>
         <div>
-          <button style={styles.simplePlusButton}>간편친구추가</button>
+          <button style={styles.simplePlusButton} onClick={simpleFriendPlus}>
+            간편친구추가
+          </button>
         </div>
       </div>
 
@@ -204,7 +297,7 @@ function WithFriends() {
                 <div style={styles.friendInfo}>
                   <p style={styles.friendNickname}>{friend.nickname}님</p>
                   <p style={styles.friendDetails}>
-                    {friend.age}/{friend.gender}/{friend.loacte}
+                    {friend.age}/{friend.gender}/{friend.locate}
                   </p>
                 </div>
                 <div>
