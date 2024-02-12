2e5792b37cadd2eb64155ec6b4ae69e76450fcc6
ndn1665 <ndn1665@daum.net>
2024-02-12 12:52:49 +0900

    aws test for kakaologin

--- catchme/mysite/common/views/common_base_views.py
+++ catchme/mysite/common/views/common_base_views.py
@@ -37,27 +37,27 @@ def introduction(request):
         else:#페이지 요청이면 자기소개 페이지로
             return render(request, "common/introduction.html")
 
-
-
 def kakaoLoginLogic(request):
-    access_token = request.session.get("access_token",None)
-    if access_token == None:
+    access_token = request.session.get("access_token",None) #어떻게 access_token을 받아올것인가.
+    if True: #access_token을 사용하지 않으면 어떻게 카카오로그인 되어있는지 확인할것인가.
         _restApiKey = '5e0af453ab97a10d3d73f26da031db2a'
-        _redirectUrl = 'http://127.0.0.1:8000/main/kakaoLoginLogicRedirect'
+        _redirectUrl = 'http://ec2-54-180-83-160.ap-northeast-2.compute.amazonaws.com:8080/main/kakaoLoginLogicRedirect'
         _url = f'https://kauth.kakao.com/oauth/authorize?client_id={_restApiKey}&redirect_uri={_redirectUrl}&response_type=code'
-        return redirect(_url)#카카오 서버로 접속하는것 여기로 접속하면 redirect uri로 정보를 쏴줌
+        return JsonResponse({'redirect_url': _url})#이 url로 접속하면 kakaoLoginLogicRedirect 로 카카오서버가 정보를 쏴주므로, 클라이언트에서 이 
     else:
+        #이미 로그인 되어있으므로 로그인 되어있다는 것을 프론트에 알려줌
+        #else부분 아직 코드 미완성
         return HttpResponse("이미 로그인 되어 있습니다.")
 
 def kakaoLoginLogicRedirect(request):
     _qs = request.GET['code']
-    _restApiKey = '5e0af453ab97a10d3d73f26da031db2a' 
-    _redirect_uri = 'http://127.0.0.1:8000/main/kakaoLoginLogicRedirect'
+    _restApiKey = '5e0af453ab97a10d3d73f26da031db2a'
+    _redirect_uri = 'http://ec2-54-180-83-160.ap-northeast-2.compute.amazonaws.com:8080/main/kakaoLoginLogicRedirect'
     _url = f'https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={_restApiKey}&redirect_uri={_redirect_uri}&code={_qs}'
     _res = requests.post(_url) # post형식으로 온 정보를
     _result = _res.json() #json화 한 후
-    request.session['access_token'] = _result['access_token']#access token만 세션에 저장
-    request.session.modified = True
+    #로그인 성공 후 access 토큰 저장 =>   _result['access_token']
+
     
     return redirect("common:introduction") #로그인 완료 후엔 home페이지로
 
