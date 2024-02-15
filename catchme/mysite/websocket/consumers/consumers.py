7b900decc0dd47d9656634544448557aa6644312
ki <redcedar1@naver.com>
2024-02-15 21:35:48 +0900

    websocket updated

--- catchme/mysite/websocket/consumers/consumers.py
+++ catchme/mysite/websocket/consumers/consumers.py
@@ -1,8 +1,12 @@
-from asgiref.sync import async_to_sync
+from asgiref.sync import async_to_sync, sync_to_async
 from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
 import json
 
+
 class DataConsumer(AsyncWebsocketConsumer):
+    # user_ready 메시지를 처리하는 메서드
+    async def user_ready(self, event):
+        pass
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'data_%s' % self.room_name
@@ -22,25 +26,46 @@ class DataConsumer(AsyncWebsocketConsumer):
             self.channel_name
         )
 
-    # Receive message from WebSocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        message_type = text_data_json.get('type')
+        kid = text_data_json.get('kid')
 
-        # Send message to room group
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'maleuser_change',
-                'message': message
-            }
-        )
+        if message_type == 'ready' and kid is not None:
+            try:
+                await self.update_user_ready(kid, True)
+            except userInfo.DoesNotExist:
+                print(f"No user with kid {kid} exists.")
+                return
 
-    # Receive message from room group
-    async def maleuser_change(self, event):
-        message = event['message']
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'user_ready',
+                    'message': f'User {kid} is ready.'
+                }
+            )
+        elif message_type == 'not_ready' and kid is not None:
+            try:
+                await self.update_user_ready(kid, False)
+            except userInfo.DoesNotExist:
+                print(f"No user with kid {kid} exists.")
+                return
 
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({
-            'message': message
-        }))
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'user_not_ready',
+                    'message': f'User {kid} is not ready.'
+                }
+            )
+
+    # 데이터베이스 업데이트 함수
+    @sync_to_async
+    def update_user_ready(self, kid, ready):
+        from common.models import userInfo, menInfo, womenInfo
+        user = userInfo.objects.get(kid=kid)
+        if user.ismale:
+            menInfo.objects.filter(user=user).update(ready=ready)
+        else:
+            womenInfo.objects.filter(user=user).update(ready=ready)
