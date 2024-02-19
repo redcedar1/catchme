a8aaec86cc5cbe0a6801a7e49f950d9ba8737e76
ndn1665 <ndn1665@daum.net>
2024-02-19 19:53:53 +0900

    w_crush choice function updated

--- catchme/mysite/common/urls.py
+++ catchme/mysite/common/urls.py
@@ -1,10 +1,15 @@
-from django.urls import path
+from django.urls import include, path
 from django.contrib.auth import views as auth_views
 from common.views import common_base_views , common_user_views
+from common.views.common_user_views import UserViewSet
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from rest_framework.routers import DefaultRouter
+
+userRouter = DefaultRouter()
+userRouter.register(r'api/user_info', UserViewSet, basename='userinfo')
 
 app_name = "common"
 urlpatterns = [
@@ -17,9 +22,10 @@ urlpatterns = [
     path('csrf/',common_base_views.csrf),
     
     #common_user_views.py
-    path('api/user_info/<str:kid>',common_user_views.UserView.as_view()),# str을 int로 바꾸는 과정 필요
     path('api/user_info/<str:kid>/notice',common_user_views.UserNoticeView.as_view()),
     
+    #changing to viewset
+    path('', include(userRouter.urls)),
     #simplejwt token
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
--- catchme/mysite/common/views/common_user_views.py
+++ catchme/mysite/common/views/common_user_views.py
@@ -1,30 +1,41 @@
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from rest_framework import generics, status
+from rest_framework.viewsets import ViewSet
 from rest_framework.views import APIView
 from common.models import *
 from common.serializers import *
+from rest_framework.viewsets import ModelViewSet
 
-class UserView(APIView):
-    def get(self,request, kid):
+class UserViewSet(ModelViewSet): #url 설정 해야함
+
+    queryset = userInfo.objects.all()
+    serializer_class = UserInfoSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        kid = kwargs.get('pk')  # 'pk'는 URL에서 전달된 객체의 식별자
         kid = int(kid)
-        userinfo = get_object_or_404(userInfo, kid = kid)
+        userinfo = get_object_or_404(userInfo, kid=kid)
         serializer = UserInfoSerializer(userinfo)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data)
     
-    def post(self, request, kid):
-        # POST 요청 처리 로직
-        # 예를 들어, 요청 데이터를 검증하고 모델을 저장하는 로직
-        kid = int(kid)
+    def update(self, request,*args, **kwargs):
+        #modal id를 부여해야함 -> 남자한테만 뜨는 모달창이기 때문
+        kid = kwargs.get('pk')
         userinfo = get_object_or_404(userInfo,kid = kid)
-        w_crush_kid_data = request.data.get("w_crush_kid")
-        if w_crush_kid_data:
-            # w_crush 속성 업데이트
-            userinfo.w_crush_kid = w_crush_kid_data
-            userinfo.save()
-            return Response({"message": "w_crush가 업데이트되었습니다."}, status=status.HTTP_200_OK)
+
+        if userinfo.ismale:
+            user = userinfo.man_userInfo.get()
+            w_crush_kid_data = request.data.get("w_crush_kid")
+
+            crush_userInfo = get_object_or_404(userInfo,kid=w_crush_kid_data)
+            user.w_crush = crush_userInfo.woman_userInfo.get()
+
+            user.save()
         else:
-            return Response({"error": "w_crush 데이터가 제공되지 않았습니다."}, status=status.HTTP_400_BAD_REQUEST)
+            pass
+        return Response({"message": "w_crush가 업데이트되었습니다."}, status=status.HTTP_200_OK)
+
 
 class UserNoticeView(generics.ListAPIView):
     def get(self, request, kid):
