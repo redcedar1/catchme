5e6dafceed94cf188bc6bca2f7ec00aff44ffc9d
ndn1665 <ndn1665@daum.net>
2024-02-19 17:29:38 +0900

    userInfo crush post configured

--- catchme/mysite/common/views/common_user_views.py
+++ catchme/mysite/common/views/common_user_views.py
@@ -11,13 +11,20 @@ class UserView(generics.ListAPIView):
         userinfo = get_object_or_404(userInfo, kid = kid)
         serializer = UserInfoSerializer(userinfo)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    """
-    def post(self,request,kid):
-        kid = int(kid)
-        return Response({"message": "처리가 완료되었습니다."},status=status.HTTP_200_OK)
-
-    """
     
+    def post(self, request, kid):
+        # POST 요청 처리 로직
+        # 예를 들어, 요청 데이터를 검증하고 모델을 저장하는 로직
+        kid = int(kid)
+        userinfo = get_object_or_404(userInfo,kid = kid)
+        w_crush_data = request.data.get("w_crush")
+        if w_crush_data:
+            # w_crush 속성 업데이트
+            userinfo.w_crush = w_crush_data
+            userinfo.save()
+            return Response({"message": "w_crush가 업데이트되었습니다."}, status=status.HTTP_200_OK)
+        else:
+            return Response({"error": "w_crush 데이터가 제공되지 않았습니다."}, status=status.HTTP_400_BAD_REQUEST)
 
 class UserNoticeView(generics.ListAPIView):
     def get(self, request, kid):
