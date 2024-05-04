4c964c7fbbd7fb5d66d8554250fdf2e8e213af34
ki <redcedar1@naver.com>
2024-05-04 19:14:25 +0900

    caching endpoint added

--- catchme/mysite/redis/redis_utils.py
+++ catchme/mysite/redis/redis_utils.py
@@ -1,3 +1,3 @@
 import redis
 
-redis_client = redis.Redis(host='your-elasticache-endpoint', port=6379, db=0)
+redis_client = redis.Redis(host='cachetest2-zyuu45.serverless.apn2.cache.amazonaws.com:6379', port=6379, db=0)
