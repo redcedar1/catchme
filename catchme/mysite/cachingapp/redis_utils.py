309542fc9b01a5e0132f30474b1728404d8afc26
ki <redcedar1@naver.com>
2024-05-04 19:32:09 +0900

    endpoint updated

--- catchme/mysite/cachingapp/redis_utils.py
+++ catchme/mysite/cachingapp/redis_utils.py
@@ -1,3 +1,3 @@
 import redis
 
-redis_client = redis.Redis(host='cachetest2-zyuu45.serverless.apn2.cachingapp.amazonaws.com:6379', port=6379, db=0)
+redis_client = redis.Redis(host='cachetest2-zyuu45.serverless.apn2.cachingapp.amazonaws.com', port=6379, db=0)
