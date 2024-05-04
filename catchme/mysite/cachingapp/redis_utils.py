56333f72012ebe955a56fb3a5c8663c6e579f8bf
ki <redcedar1@naver.com>
2024-05-04 19:24:16 +0900

    cachingapp updated

---
+++ catchme/mysite/cachingapp/redis_utils.py
@@ -0,0 +1,3 @@
+import redis
+
+redis_client = redis.Redis(host='cachetest2-zyuu45.serverless.apn2.cachingapp.amazonaws.com:6379', port=6379, db=0)
--- catchme/mysite/redis/redis_utils.py
+++
@@ -1,3 +0,0 @@
-import redis
-
-redis_client = redis.Redis(host='cachetest2-zyuu45.serverless.apn2.cache.amazonaws.com:6379', port=6379, db=0)
